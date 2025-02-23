49aa1d523b2dfbd9cba0186fd2dfcc6d6f59240f
Ben Randall <bhdrandall@gmail.com>
2025-02-23 19:02:28 +0000

    abstract search to controller

--- Controllers/SearchController.cs
+++ Controllers/SearchController.cs
@@ -2,20 +2,60 @@
 using Microsoft.AspNetCore.Mvc.ViewEngines;
 using Umbraco.Cms.Core.Web;
 using Umbraco.Cms.Web.Common.Controllers;
+using Examine;
+using Examine.Search;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Web.Common.PublishedModels;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+using Azure;
+using Umbraco.Cms.Core.Models.PublishedContent;
+using UmbracoDemo.ViewModels;
 
 namespace UmbracoDemo.Controllers
 {
 	public class SearchController : RenderController
 	{
-		public SearchController(ILogger<SearchController> logger, ICompositeViewEngine compositeViewEngine, IUmbracoContextAccessor umbracoContextAccessor)
+		private readonly IExamineManager _examineManager;
+		private readonly IVariationContextAccessor _variationContextAccessor;
+		private readonly ServiceContext _serviceContext;
+
+		public SearchController(ILogger<SearchController> logger, ICompositeViewEngine compositeViewEngine, IUmbracoContextAccessor umbracoContextAccessor, IExamineManager examineManager, IVariationContextAccessor variationContextAccessor, ServiceContext context)
 		: base(logger, compositeViewEngine, umbracoContextAccessor)
 		{
+			_examineManager = examineManager;
+			_variationContextAccessor = variationContextAccessor;
+			_serviceContext = context;
 		}
 
 		public override IActionResult Index()
 		{
+			var searchQuery = HttpContext.Request.Query["query"].ToString();
+			var searchResults = Enumerable.Empty<ISearchResult>();
 
-			return CurrentTemplate(CurrentPage);
+			if (!string.IsNullOrWhiteSpace(searchQuery))
+			{
+				if (_examineManager.TryGetIndex("ExternalIndex", out var index))
+				{
+					var searcher = index.Searcher;
+
+					var query = searcher.CreateQuery("content")
+						.Field("nodeName", searchQuery.Fuzzy(0.5f))
+						.Or()
+						.Field("content", searchQuery.Fuzzy(0.5f))
+						.Not()
+					.Field("includeInSearch", "0");
+					searchResults = query.Execute();
+				}
+			}
+
+			var searchViewModel = new SearchViewModel(CurrentPage, new PublishedValueFallback(_serviceContext, _variationContextAccessor))
+			{
+				SearchQuery = searchQuery,
+				SearchResults = searchResults
+			};
+
+			return CurrentTemplate(searchViewModel);
 		}
 	}
 }
---
+++ ViewModels/SearchViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Examine;
+using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Web.Common.PublishedModels;
+using UmbracoDemo.Models;
+
+namespace UmbracoDemo.ViewModels
+{
+	public class SearchViewModel : Search
+	{
+		public SearchViewModel(IPublishedContent content, IPublishedValueFallback publishedValueFallback) : base(content, publishedValueFallback)
+		{
+		}
+
+		public string? SearchQuery { get; set; }
+		public IEnumerable<ISearchResult> SearchResults { get; set; }
+	}
+}
